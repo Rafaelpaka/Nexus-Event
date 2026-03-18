553df312ceeeea77fc42a42c6053aa6acd125eab
Rafaelpaka <rafael24un@gmail.com>
2026-03-18 20:51:49 -0300

    Update IngressoEntity.cs
    Fix: Entities IngressoEntity
    
    Error verification

--- Nexus-Event/src/backend/Entities/IngressoEntity.cs
+++ Nexus-Event/src/backend/Entities/IngressoEntity.cs
@@ -1,4 +1,4 @@
-namespace backend.Entities.IngressoEntity;
+namespace backend.Entities;
 
 public class IngressoEntity
 {
@@ -6,10 +6,18 @@ public class IngressoEntity
     public IngressoEntity(int valor, int idUsuario)
     {
         if (valor <= 0)
-            throw 'Não é possível a criação de um ingresso com valor igual a 0 ou menor';
+            throw new ArgumentOutOfRangeException(nameof(valor), "O valor deve ser maior que zero.");
+
+        if (idUsuario <= 0)
+            throw new ArgumentOutOfRangeException(nameof(idUsuario), "O ID do usuário deve ser válido.");
+
+        IdUsuario = idUsuario;
+        Valor = valor;
+        DataCriação = DateTime.Now;
+
     }
 
-
+    public int IdUsuario { get; }
     public int Id { get; set; }
     public DateTime DataCriação { get; set; }
     public string Codigo { get; set; }
