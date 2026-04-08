9ed7227754a1e1af71e58910be0c110880916060
Larissarff <larissa.rfferreira4800@gmail.com>
2026-04-07 21:15:48 -0300

    feat: add instances in program.cs

--- Nexus-Event/src/frontend/Program.cs
+++ Nexus-Event/src/frontend/Program.cs
@@ -1,18 +1,22 @@
 using frontend.Components;
+using backend.Services;
+using backend.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<UsuarioRepository>(sp => new UsuarioRepository(connectionString!));
+builder.Services.AddScoped<UsuarioService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
